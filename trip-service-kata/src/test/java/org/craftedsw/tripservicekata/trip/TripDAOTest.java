13403c277dbfb7f804da4bac8320c83f22271f86
dfneto <pdavidfneto@gmail.com>
2015-10-07 19:56:37 -0300

    tripsBy(user) adicionado a TripDAO()

--- trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
+++ trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripDAOTest.java
@@ -1,5 +1,15 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.CollaboratorCallException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Test;
+
 public class TripDAOTest {
+
+    @Test(expected = CollaboratorCallException.class) public void
+    should_throw_an_exception_when_trips_are_returned() {
+        new TripDAO().tripsBy(new User());
+
+    }
 
 }
